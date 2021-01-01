64e0137318b8dae0ec83ad59127110850c2db9db
Anthony Wang <ta180m@gmail.com>
2021-01-01 10:54:57 -0600

    Add hints for G, H, I

---
+++ E/gen.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Prepare RNG
+    default_random_engine generator(chrono::system_clock::now().time_since_epoch().count());
+    normal_distribution<double> distribution(100.0, 15.0); // Totally not IQ
+
+    // Prepare I/O
+    freopen("D.dat", "w", stdout);
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    vector<string> words({ "dog", "cat", ""})
+
+
+    const int N = 1e6;
+    for (int i = 0; i < N; ++i) {
+        if (i == 54321) cout << 300 << ' ' << "300iq" << '\n';
+        else cout << distribution(generator) << ' ' << words[rand()%words.size()] << '\n';
+    }
+}
