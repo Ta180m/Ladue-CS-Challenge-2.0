c29e6e08508a0f0d0ad55366ae0e596dc515d842
Anthony Wang <ta180m@gmail.com>
2021-01-01 11:15:03 -0600

    Finished E

--- E/gen.cpp
+++ E/gen.cpp
@@ -4,18 +4,17 @@ using namespace std;
 int main() {
     // Prepare RNG
     default_random_engine generator(chrono::system_clock::now().time_since_epoch().count());
-    normal_distribution<double> distribution(100.0, 15.0); // Totally not IQ
+    normal_distribution<double> distribution(5000.0, 1000.0);
 
     // Prepare I/O
-    freopen("D.dat", "w", stdout);
+    freopen("E.dat", "w", stdout);
     ios_base::sync_with_stdio(0), cin.tie(0);
 
-    vector<string> words({ "dog", "cat", ""})
-
+    vector<string> words({ "penguin", "gnu", "fox", "mouse", "coyote", "mule", "platypus", "squirrel", "monkey" });
 
     const int N = 1e6;
     for (int i = 0; i < N; ++i) {
-        if (i == 54321) cout << 300 << ' ' << "300iq" << '\n';
+        if (i % 20000 == 10000) cout << 1729+(rand()%20)-10 << ' ' << "gnu" << '\n';
         else cout << distribution(generator) << ' ' << words[rand()%words.size()] << '\n';
     }
 }
