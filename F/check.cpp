6d6354b0d74b1716721475c225932fd3d4a0446b
Anthony Wang <ta180m@gmail.com>
2021-01-01 12:08:28 -0600

    Finish F

---
+++ F/check.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ifstream cin("F.dat");
+    ios_base::sync_with_stdio(0), cin.tie(0);
+
+    vector<double> vc;
+    for (int i = 0; i < 1e6; ++i) {
+        double x;
+        cin >> x;
+        vc.push_back(x);
+    }
+
+    double ans = 1e9;
+    sort(begin(vc), end(vc));
+    for (int i = 0; i < vc.size()-1; ++i) {
+        ans = min(vc[i+1]-vc[i], ans);
+        if (vc[i] == vc[i+1]) cout << 1 << '\n';
+    }
+    cout << setprecision(12) << ans;
+}
